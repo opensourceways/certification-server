f691ea15e7e5cc2aed0c758619599ac8bc26d2f1
ssignik <364208960@qq.com>
2024-08-14 19:03:27 +0800

    refresh cookie api

--- openeulerSever/src/main/java/com/huawei/it/euler/config/usercenter/TokenConfig.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/config/usercenter/TokenConfig.java
@@ -27,6 +27,8 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -115,7 +117,8 @@ public class TokenConfig {
             return;
         }
         String uuid = null;
-        String token = null;
+        String ut_key = null;
+        String yg_key = null;
         for (Cookie cookie : cookies) {
             if (cookie == null) {
                 continue;
@@ -124,10 +127,13 @@ public class TokenConfig {
                 uuid = cookie.getValue();
             }
             if (Objects.equals(cookie.getName(), "_U_T_")) {
-                token = cookie.getValue();
+                ut_key = cookie.getValue();
+            }
+            if (Objects.equals(cookie.getName(), "_Y_G_")) {
+                yg_key = cookie.getValue();
             }
         }
-        if (StringUtils.isEmpty(uuid) || StringUtils.isEmpty(token)) {
+        if (StringUtils.isEmpty(uuid) || StringUtils.isEmpty(ut_key) || StringUtils.isEmpty(ut_key)) {
             return;
         }
         String enUuid = encryptUtils.aesEncrypt(uuid);
@@ -140,14 +146,26 @@ public class TokenConfig {
         if (currentTimeMillis > tokenRefreshTime) {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            headers.set("token", token);
-            log.debug("refresh cookie token :{}", token);
+            headers.set("token", ut_key);
+            List<String> cookieList = new ArrayList<>();
+            cookieList.add("_Y_G_=" + yg_key);
+            headers.put(HttpHeaders.COOKIE, cookieList);
             HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
             ResponseEntity<JSONObject> responseEntity = restTemplate.getForEntity(refreshTokenUrl, JSONObject.class, httpEntity);
             JSONObject refreshData = responseEntity.getBody();
             log.debug("refresh api code :{}", responseEntity.getStatusCode());
             log.debug("refresh api data :{}", refreshData.toJSONString());
-            int tokenIntervalMin = refreshData.getJSONObject("data").getInteger("tokenExpireInterval");
+            JSONObject dataJSONObject = refreshData.getJSONObject("data");
+            if (dataJSONObject == null) {
+                return;
+            }
+            int tokenIntervalMin;
+            if (dataJSONObject.containsKey("token_expires_in")) {
+                tokenIntervalMin = dataJSONObject.getInteger("token_expires_in");
+            } else {
+                // default 30 min
+                tokenIntervalMin = 30 * 60;
+            }
             // half of tokenIntervalMin times passed then refresh the token
             long newTokenRefreshTime = tokenIntervalMin / 2 * 1000L + System.currentTimeMillis();
             String finalUuid = uuid;
