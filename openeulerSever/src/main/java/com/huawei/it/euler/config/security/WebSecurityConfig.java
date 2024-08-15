56fbdb1627a73ba09c87e5f5157181604e4c5146
ssignik <364208960@qq.com>
2024-08-15 21:56:01 +0800

    disable default security header

--- openeulerSever/src/main/java/com/huawei/it/euler/config/security/WebSecurityConfig.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/config/security/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -84,7 +85,7 @@ public class WebSecurityConfig {
                 .exceptionHandling(exceptions ->
                         exceptions.accessDeniedHandler(eulerAccessDeniedHandler)
                         .authenticationEntryPoint(eulerAuthenticationEntryPoint))
-                .headers(HeadersConfigurer::defaultsDisabled)
+                .headers(headers -> headers.defaultsDisabled().cacheControl(Customizer.withDefaults()))
 //                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(csrfFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
