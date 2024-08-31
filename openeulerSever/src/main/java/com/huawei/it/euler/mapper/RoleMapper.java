60ad5d806a7155924bb66bbbc468f234867d7d4f
LiYanghang00 <2623197090@qq.com>
2024-08-31 10:52:12 +0800

    update software_t

--- openeulerSever/src/main/java/com/huawei/it/euler/mapper/RoleMapper.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/mapper/RoleMapper.java
@@ -28,7 +28,7 @@ public interface RoleMapper {
 
     void insertDefaultRole(@Param("userId") Integer userId);
 
-    List<Integer> findUserByRole(@Param("roleId") Integer roleId);
+    List<Integer> findUserByRole(@Param("roleId") Integer roleId,@Param("softwareId") Integer softwareId);
 
     List<RoleVo> findRoleByUserId(@Param("uuid") Integer uuid,@Param("roleId") Integer roleId);
 }
