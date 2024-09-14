93c9c88f0a4b0acc738ef1ba1992494d46de2a25
ssignik <364208960@qq.com>
2024-09-14 16:04:08 +0800

    1. add license 2. add comment

--- openeulerSever/src/main/java/com/huawei/it/euler/ddd/domain/account/Role.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/domain/account/Role.java
@@ -1,13 +1,31 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
 package com.huawei.it.euler.ddd.domain.account;
 
 import lombok.Data;
 
+/**
+ * 角色信息
+ *
+ * @since 2024/09/14
+ */
 @Data
 public class Role {
+    /**
+     * 角色id
+     */
     private int id;
 
+    /**
+     * 角色值
+     */
     private String role;
 
+    /**
+     * 角色名称
+     */
     private String roleName;
 
 }
--- openeulerSever/src/main/java/com/huawei/it/euler/ddd/infrastructure/cache/AutoLoadUserToCacheService.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/infrastructure/cache/AutoLoadUserToCacheService.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
 package com.huawei.it.euler.ddd.infrastructure.cache;
 
 import com.huawei.it.euler.ddd.domain.account.RoleService;
