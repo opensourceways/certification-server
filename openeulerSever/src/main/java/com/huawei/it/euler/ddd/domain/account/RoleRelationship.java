539dbc832e4881b4ea20d0fbee75fa3af6b92fa2
ssignik <364208960@qq.com>
2024-09-14 14:23:23 +0800

    user center logic refactor

---
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/domain/account/RoleRelationship.java
@@ -0,0 +1,12 @@
+package com.huawei.it.euler.ddd.domain.account;
+
+import lombok.Data;
+
+@Data
+public class RoleRelationship {
+
+    private String uuid;
+
+    private int roleId;
+
+}
---
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/infrastructure/oidc/OidcCookie.java
@@ -0,0 +1,12 @@
+package com.huawei.it.euler.ddd.infrastructure.oidc;
+
+import lombok.Data;
+
+@Data
+public class OidcCookie {
+
+    private String _U_T_;
+
+    private String _Y_G_;
+
+}
