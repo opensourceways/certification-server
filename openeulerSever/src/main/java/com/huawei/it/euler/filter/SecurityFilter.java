db8b9ecfcde22c5d350c95072dca0876c008adef
LiYanghang <61582019+LiYanghang00@users.noreply.github.com>
2024-09-19 14:36:33 +0800

    测试用例 (#41)
    * filterCriteria
    
    * recover api of /software/communityCheckList
    fixed error
    
    * add audit log
    
    * add audit log
    
    * update cookie
    
    * update sql
    
    * update sql
    
    * update user
    
    * update user
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * update software_t
    
    * 流程修改1
    
    * 流程修改2
    
    * 流程修改3
    
    * 流程修改4
    
    * 修改列表查询
    
    * 修改列表查询
    
    * 修改列表查询
    
    * log
    
    * log
    
    * user auth refactor
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * merge user refactor in dev
    
    * log
    
    * log
    
    * log
    
    * update user
    
    * deal with null scenario when get user info from user center
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * log
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    * 测试用例
    
    ---------
    
    Co-authored-by: ssignik <364208960@qq.com>

--- openeulerSever/src/main/java/com/huawei/it/euler/filter/SecurityFilter.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/filter/SecurityFilter.java
@@ -46,6 +46,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        LOGGER.debug("url : {}", request.getRequestURL());
         String shortUri = RequestUtils.getShortUri(request);
         // 校验referer
         int checkReferer = checkReferer(request, shortUri);
