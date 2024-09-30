009bbde9fc5b8a718b604c0637862557b87e98f5
LiYanghang00 <2623197090@qq.com>
2024-09-30 17:09:23 +0800

    easyexcel

--- openeulerSever/src/main/java/com/huawei/it/euler/model/dto/SoftwareDTO.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/model/dto/SoftwareDTO.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
 import com.alibaba.excel.annotation.ExcelProperty;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.huawei.it.euler.model.vo.ComputingPlatformVo;
 
 import lombok.Data;
@@ -24,7 +23,7 @@ public class SoftwareDTO {
     /**
      * 软件id
      */
-    @ExcelProperty("软件id")
+    @ExcelProperty("流程序号")
     private Integer id;
 
     /**
@@ -36,114 +35,84 @@ public class SoftwareDTO {
     /**
      * 产品名称
      */
+    @ExcelProperty("产品名称")
     private String productName;
 
     /**
      * 产品类型
      */
+    @ExcelProperty("产品类型")
     private String productType;
 
     /**
      * 产品版本
      */
+    @ExcelProperty("产品版本")
     private String productVersion;
 
     /**
      * os名称
      */
+    @ExcelProperty("os名称")
     private String osName;
 
     /**
      * os版本
      */
+    @ExcelProperty("os版本")
     private String osVersion;
-
-    /**
-     * 获取算力平台集合字符串
-     */
-    @JsonIgnore
-    private String hashratePlatform;
 
     /**
      * 算力平台集合
      */
+    @ExcelProperty("算力平台集合")
     private List<ComputingPlatformVo> hashratePlatformList;
 
     /**
      * 测试机构
      */
+    @ExcelProperty("测试机构")
     private String testOrganization;
-
-    /**
-     * 测试机构ID
-     */
-    private Integer testOrgId;
 
     /**
      * 申请时间
      */
+    @ExcelProperty("申请时间")
     private String applicationTime;
-
-    /**
-     * 申请人ID
-     */
-    private String applicant;
 
     /**
      * 申请人姓名
      */
+    @ExcelProperty("申请人姓名")
     private String applicantName;
-
-    /**
-     * 审核人ID
-     */
-    private String reviewer;
 
     /**
      * 审核人姓名
      */
+    @ExcelProperty("审核人姓名")
     private String reviewerName;
 
-    @JsonIgnore
-    private Integer reviewRole;
-    /**
-     * 前审核人Id
-     */
-    private String preReviewer;
     /**
      * 认证状态
      */
+    @ExcelProperty("认证状态")
     private String status;
 
-    private Integer statusId;
     /**
      * 算力平台名称
      */
+    @ExcelProperty("算力平台名称")
     private String hashratePlatformaNameList;
 
     /**
      * 硬件厂商
      */
+    @ExcelProperty("硬件厂商")
     private String cpuVendor;
-
-    /**
-     * 已驳回字段
-     */
-    @JsonIgnore
-    private String authenticationStatus;
-
-    /**
-     * 操作
-     */
-    private String operation;
-
-    /**
-     * 测评场景id
-     */
-    private Integer asId;
 
     /**
      * 认证完成时间
      */
+    @ExcelProperty("认证完成时间")
     private String certificationTime;
 }
