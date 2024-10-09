dfd35b8a22e905a8a01b0d8081219edc53f0dcdc
LiYanghang00 <2623197090@qq.com>
2024-10-09 14:40:04 +0800

    software update

--- openeulerSever/src/main/java/com/huawei/it/euler/controller/converter/SoftwareVOToDTOConverter.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/controller/converter/SoftwareVOToDTOConverter.java
@@ -2,9 +2,11 @@
  * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
  */
 
-package com.huawei.it.euler.model.converter;
+package com.huawei.it.euler.controller.converter;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import com.huawei.it.euler.model.dto.SoftwareDTO;
@@ -14,5 +16,9 @@ import com.huawei.it.euler.util.Converter;
 @Mapper
 public  abstract class SoftwareVOToDTOConverter implements Converter<SoftwareVo, SoftwareDTO> {
     public static final SoftwareVOToDTOConverter INSTANCE = Mappers.getMapper( SoftwareVOToDTOConverter.class );
+
+    @Override
+    @Mappings({ @Mapping(source = "userUuid", target = "id")})
+    public abstract SoftwareDTO convert(SoftwareVo source);
 }
 
