06883214f8c4791a18a11ec9c6ef01d56f74e9b7
LiYanghang00 <2623197090@qq.com>
2024-10-14 10:39:06 +0800

    software update

--- openeulerSever/src/main/java/com/huawei/it/euler/ddd/service/UserRoleService.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/service/UserRoleService.java
@@ -7,10 +7,10 @@ package com.huawei.it.euler.ddd.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.huawei.it.euler.ddd.domain.account.UserRole;
 import com.huawei.it.euler.ddd.domain.account.UserRoleVO;
+import com.huawei.it.euler.ddd.domain.account.converter.UserRoleToVOConverter;
 import com.huawei.it.euler.ddd.domain.account.query.UserRoleQuery;
 import com.huawei.it.euler.ddd.domain.account.repository.UserRoleRepository;
 import com.huawei.it.euler.model.vo.PageResult;
@@ -23,10 +23,7 @@ public class UserRoleService {
 
     public PageResult<UserRoleVO> getAccountRoleListByQuery(String uuid, UserRoleQuery query, int curPage, int pageSize) {
         query.setUuid(uuid);
-        QueryWrapper<UserRole> queryWrapper = new QueryWrapper<>();
-        Page<UserRole> poPage = new Page<>(curPage, pageSize);
-        Page<UserRole> page = userRoleRepository.page(poPage, queryWrapper);
-        PageResult<UserRoleVO> pageResult = new PageResult<>();
-        return null;
+        Page<UserRole> page = userRoleRepository.getUserRolePage(query, curPage, pageSize);
+        return UserRoleToVOConverter.INSTANCE.convert(page);
     }
 }
