c68cb1b4275beddef59ab7dd440bb2857b210977
LiYanghang00 <2623197090@qq.com>
2024-10-25 15:16:49 +0800

    bugfix

--- openeulerSever/src/main/java/com/huawei/it/euler/controller/converter/SoftwareVOToDTOConverter.java
+++ openeulerSever/src/main/java/com/huawei/it/euler/controller/converter/SoftwareVOToDTOConverter.java
@@ -7,16 +7,19 @@ package com.huawei.it.euler.controller.converter;
 import com.huawei.it.euler.model.dto.SoftwareDTO;
 import com.huawei.it.euler.model.vo.SoftwareVo;
 import com.huawei.it.euler.util.Converter;
+import com.huawei.it.euler.util.ConverterUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = ConverterUtils.class)
 public abstract class SoftwareVOToDTOConverter implements Converter<SoftwareVo, SoftwareDTO> {
     public static final SoftwareVOToDTOConverter INSTANCE = Mappers.getMapper(SoftwareVOToDTOConverter.class);
 
     @Override
-    @Mappings({@Mapping(source = "userUuid", target = "applicant")})
+    @Mappings({@Mapping(source = "userUuid", target = "applicant"),
+            @Mapping(source = "certificationTime", target = "certificationTime", qualifiedByName = "dateToString"),
+            @Mapping(source = "applicationTime", target = "applicationTime", qualifiedByName = "dateToString")})
     public abstract SoftwareDTO convert(SoftwareVo source);
 }
