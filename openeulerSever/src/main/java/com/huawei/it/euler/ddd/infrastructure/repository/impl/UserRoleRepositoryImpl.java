d1ec057d2192dfd9b22ac2959927a3ce1720eb22
ssignik <364208960@qq.com>
2024-11-11 11:28:09 +0800

    1.add role entity and repository

---
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/infrastructure/repository/impl/UserRoleRepositoryImpl.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.it.euler.ddd.infrastructure.repository.impl;
+
+/**
+ * 用户角色 持久化接口实现类
+ *
+ * @author zhaoyan
+ * @since 2024-11-07
+ */
+public class UserRoleRepositoryImpl {
+}
---
+++ openeulerSever/src/main/java/com/huawei/it/euler/ddd/infrastructure/repository/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.it.euler.ddd.infrastructure.repository.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 角色 Mapper 接口
+ *
+ * @author zhaoyan
+ * @since 2024-11-07
+ */
+public interface RoleMapper extends BaseMapper<RolePO> {
+}
